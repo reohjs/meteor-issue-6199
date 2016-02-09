1d0f8302d22747bca5e8f9bfda1112cd5319f7ee
reohjs <jazzysenpai@gmail.com>
2016-02-09 00:08:44 +0000

    Fix syntax

--- server/main.js
+++ server/main.js
@@ -3,6 +3,6 @@ import {BrowserPolicy} from 'meteor/browser-policy';
 
 console.log('BrowserPolicy is: ', typeof BrowserPolicy);
 
-import Test from 'meteor/user:main-test';
+import {Test} from 'meteor/user:main-test';
 
 console.log('Test is: ', typeof Test);
